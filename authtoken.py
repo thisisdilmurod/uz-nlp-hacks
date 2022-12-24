f8cf0bad92b4cb8f107fa4617d2025c7f6b557e4
Dilmurod Abdusamadov <100064552+thisisdilmurod@users.noreply.github.com>
2022-12-24 20:09:53 +0300

    Create authtoken.py

---
+++ authtoken.py
@@ -0,0 +1,5 @@
+# Replicate with API token
+# Set it as environmental variable
+# Read the description for more info
+# https://replicate.com/docs
+auth_token = "09d85946232c6d2735baed1e65e32b04a2b256d7"
